367aeb52a7e7e62a0c0117177ca28cdc9bafff79
Radia tiyan <radiatiyan92@gmail.com>
2022-03-23 17:54:29 +0700

    yey

--- api/routes/user.js
+++ api/routes/user.js
@@ -8,35 +8,6 @@ const User = require('../models/user');
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
 
-const storageConf = multer.diskStorage({
-    destination: function (req, res, cb) {
-        cb(null, './uploads/');
-    },
-    filename: function (req, file, cb) {
-
-        const uniquePrefix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-        cb(null, uniquePrefix + '-' + file.originalname)
-    }
-})
-
-const fileFilter = (req, file, cb) => {
-    //reject File
-    if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
-        cb(null, true);
-    } else {
-        cb(new Error('File not found'), false);
-    }
-}
-
-const upload = multer({
-    storage: storageConf,
-    limits: {
-        fileSize: 1024 * 1024 * 2
-    },
-    fileFilter: fileFilter
-});
-
-
 router.post('/', (req, res, next) => {
 
     User.find({ email: req.body.email })
@@ -55,17 +26,8 @@ router.post('/', (req, res, next) => {
                     } else {
                         const user = new User({
                             _id: new mongoose.Types.ObjectId(),
-                            nama_depan: req.body.nama_depan,
-                            nama_belakang: req.body.nama_belakang,
-                            tentang_toko: req.body.tentang_toko,
                             email: req.body.email,
-                            password: hash,
-                            nomor_hp: req.body.nomor_hp,
-                            negara: req.body.negara,
-                            bahasa: req.body.bahasa,
-                            mata_uang: req.body.mata_uang,
-                            npwp: req.body.npwp,
-                            foto: req.file.path
+                            password: hash
                         }).save()
                             .then(result => {
                                 console.log(result);
